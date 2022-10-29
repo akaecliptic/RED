7a5abb2f67d9b0dca102cc691920eaa8f2d1a264
akaecliptic <michealfatoye@gmail.com>
2022-10-29 02:52:40 +0100

    Version 0.2.4 - Isolated duplicate code to function. Added comments and TODO.

--- Movie/src/main/java/dev/akaecliptic/core/MovieFactory.java
+++ Movie/src/main/java/dev/akaecliptic/core/MovieFactory.java
@@ -13,6 +13,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Hello, this class is responsible for converting the raw JSON values to their corresponding class abstractions.
+ */
 public class MovieFactory {
     private static final Gson gson;
 
@@ -24,6 +27,8 @@ public class MovieFactory {
         builder.registerTypeAdapter(Configuration.class, new ConfigurationDeserializer());
         gson = builder.create();
     }
+
+    /* EXPOSED METHODS */
 
     public static Movie create(JsonElement json) {
         return gson.fromJson(json, Movie.class);
@@ -60,6 +65,8 @@ public class MovieFactory {
         return gson.fromJson(json, Page.class);
     }
 
+    /* INTERNAL METHODS */
+
     private static int getInt(JsonElement json) {
         if(json == null) return -1;
 
@@ -94,8 +101,8 @@ public class MovieFactory {
         return json.getAsJsonArray();
     }
 
-    private static int format(float vote_average) {
-        return (vote_average == -1) ? -1 : Math.round(vote_average * 10);
+    private static int formatRating(float rating) {
+        return (rating == -1) ? -1 : Math.round(rating * 10);
     }
 
     private static Information parseInformation(JsonObject json) {
@@ -129,23 +136,29 @@ public class MovieFactory {
         return genres;
     }
 
+    private static Movie parseMovie(JsonObject json) {
+        int id = getInt(json.get("id"));
+        boolean seen = false;
+        String title = getString(json.get("title"));
+        String description = getString(json.get("overview"));
+        int nativeRating = formatRating(getFloat(json.get("vote_average")));
+        int userRating = 0;
+        String stringDate = getString(json.get("release_date"));
+        LocalDate release = (stringDate == null) ? Movie.INDEFINITE_DATE : LocalDate.parse(stringDate);
+
+        Information information = parseInformation(json);
+
+        return new Movie(id, title, seen, description, nativeRating, userRating, release, information);
+    }
+
+    /* DESERIALIZERS */
+    // TODO: 2022-10-29 Revisit this, these seem redundant. Refactor for 0.2.6
+
     private static class MovieDeserializer implements JsonDeserializer<Movie> {
         @Override
         public Movie deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
             JsonObject json = element.getAsJsonObject();
-
-            int id = getInt(json.get("id"));
-            boolean seen = false;
-            String title = getString(json.get("title"));
-            String description = getString(json.get("overview"));
-            int nativeRating = format(getFloat(json.get("vote_average")));
-            int userRating = 0;
-            String stringDate = getString(json.get("release_date"));
-            LocalDate release = (stringDate == null) ? Movie.INDEFINITE_DATE : LocalDate.parse(stringDate);
-
-            Information information = parseInformation(json);
-
-            return new Movie(id, title, seen, description, nativeRating, userRating, release, information);
+            return parseMovie(json);
         }
     }
 
@@ -157,22 +170,32 @@ public class MovieFactory {
 
             for (int i = 0; i < array.size(); i++) {
                 JsonObject json = array.get(i).getAsJsonObject();
-
-                int id = getInt(json.get("id"));
-                boolean seen = false;
-                String title = getString(json.get("title"));
-                String description = getString(json.get("overview"));
-                int nativeRating = format(getFloat(json.get("vote_average")));
-                int userRating = 0;
-                String stringDate = getString(json.get("release_date"));
-                LocalDate release = (stringDate == null) ? Movie.INDEFINITE_DATE : LocalDate.parse(stringDate);
-
-                Information information = parseInformation(json);
-
-                movies[i] = new Movie(id, title, seen, description, nativeRating, userRating, release, information);
+                movies[i] = parseMovie(json);
             }
 
             return movies;
+        }
+
+    }
+
+    private static class PageDeserializer implements JsonDeserializer<Page> {
+        @Override
+        public Page deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject page = element.getAsJsonObject();
+            if(page == null || page.isJsonNull()) return null;
+
+            JsonArray array = page.getAsJsonArray("results");
+            List<Movie> movies = new ArrayList<>();
+
+            for (int i = 0; i < array.size(); i++) {
+                JsonObject json = array.get(i).getAsJsonObject();
+                movies.add(parseMovie(json));
+            }
+
+            int number = getInt(page.get("page"));
+            int total = getInt(page.get("total_pages"));
+
+            return new Page(number, total, movies);
         }
 
     }
@@ -198,40 +221,6 @@ public class MovieFactory {
             posterSizes.forEach(child -> posters.add(getString(child)));
 
             return new Configuration(base, secure, backdrops.toArray(new String[]{}), posters.toArray(new String[]{}));
-        }
-
-    }
-
-    private static class PageDeserializer implements JsonDeserializer<Page> {
-        @Override
-        public Page deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
-            JsonObject page = element.getAsJsonObject();
-            if(page == null || page.isJsonNull()) return null;
-
-            JsonArray array = page.getAsJsonArray("results");
-            List<Movie> movies = new ArrayList<>();
-
-            for (int i = 0; i < array.size(); i++) {
-                JsonObject json = array.get(i).getAsJsonObject();
-
-                int id = getInt(json.get("id"));
-                boolean seen = false;
-                String title = getString(json.get("title"));
-                String description = getString(json.get("overview"));
-                int nativeRating = format(getFloat(json.get("vote_average")));
-                int userRating = 0;
-                String stringDate = getString(json.get("release_date"));
-                LocalDate release = (stringDate == null) ? Movie.INDEFINITE_DATE : LocalDate.parse(stringDate);
-
-                Information information = parseInformation(json);
-
-                movies.add(new Movie(id, title, seen, description, nativeRating, userRating, release, information));
-            }
-
-            int number = getInt(page.get("page"));
-            int total = getInt(page.get("total_pages"));
-
-            return new Page(number, total, movies);
         }
 
     }
